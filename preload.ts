6584212a981eb168de9bb82ac083c6917782044b
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-06-23 07:41:45 +0000

    Deploying to gh-pages from @ flokleiser/UI-Box-Typescript@b8de2b21e05c0d8434ded97c883d7551616d474c 🚀

---
+++ preload.ts
@@ -0,0 +1,20 @@
+/**
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+
+// const { contextBridge, ipcRenderer} = require('electron/renderer')
+
+// contextBridge.exposeInMainWorld('electron', {
+//     ipcRenderer: ipcRenderer,
+// });
+
+// contextBridge.exposeInMainWorld('darkMode', {
+//   toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+//   system: () => ipcRenderer.invoke('dark-mode:system'),
+//   getThemeSource: () => ipcRenderer.invoke('dark-mode:get-theme-source')
+// })
