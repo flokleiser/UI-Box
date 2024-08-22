3fef36fd78b88637952a6260e7a5da45f9c1feff
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-08-22 15:57:58 +0200

    redoing the musializer to fix the stupid div

--- src/dist/pages/Musializer.js
+++ src/dist/pages/Musializer.js
@@ -1,45 +1,48 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Musializer;
-const react_1 = __importDefault(require("react"));
-const react_2 = require("react");
+const react_1 = __importStar(require("react"));
 const framer_motion_1 = require("framer-motion");
 const Slider_1 = require("../components/Slider");
 function Musializer() {
-    const [isPlaying, setIsPlaying] = (0, react_2.useState)(true);
-    const [volume, setVolume] = (0, react_2.useState)(50);
-    const [test, setTest] = (0, react_2.useState)(0);
-    const [bass, setBass] = (0, react_2.useState)(false);
-    const [audioData, setAudioData] = (0, react_2.useState)(new Uint8Array(0));
-    const audioRef = (0, react_2.useRef)(null);
-    const analyserRef = (0, react_2.useRef)(null);
-    const audioContextRef = (0, react_2.useRef)(null);
-    const [bassIntensity, setBassIntensity] = (0, react_2.useState)(0);
-    const canvasRef = (0, react_2.useRef)(null);
-    const [resetTrigger, setResetTrigger] = (0, react_2.useState)(0);
-    (0, react_2.useEffect)(() => {
-        const handleThemeToggle = () => resetScene();
-        setTimeout(() => {
-            const darkmodeToggleButton = document.getElementById('darkmodeToggleButton');
-            if (darkmodeToggleButton) {
-                darkmodeToggleButton.addEventListener('click', handleThemeToggle);
-            }
-        }, 1000);
-        return () => {
-            const darkmodeToggleButton = document.getElementById('darkmodeToggleButton');
-            if (darkmodeToggleButton) {
-                darkmodeToggleButton.removeEventListener('click', handleThemeToggle);
-            }
-        };
-    }, []);
-    (0, react_2.useEffect)(() => {
+    const [isPlaying, setIsPlaying] = (0, react_1.useState)(true);
+    const [volume, setVolume] = (0, react_1.useState)(50);
+    const [bass, setBass] = (0, react_1.useState)(false);
+    const [test, setTest] = (0, react_1.useState)(0);
+    const [audioData, setAudioData] = (0, react_1.useState)(new Uint8Array(0));
+    const [bassIntensity, setBassIntensity] = (0, react_1.useState)(0);
+    const canvasRef = (0, react_1.useRef)(null);
+    const audioRef = (0, react_1.useRef)(null);
+    const analyserRef = (0, react_1.useRef)(null);
+    const audioContextRef = (0, react_1.useRef)(null);
+    (0, react_1.useEffect)(() => {
         if (!audioRef.current) {
             audioRef.current = new Audio("./media/sounds/check1.mp3");
-            // audioRef.current = new Audio("./media/sounds/didITellYou.mp3") 
-            audioContextRef.current = new (window.AudioContext || window.webkitAudioContext)();
+            audioContextRef.current = new (window.AudioContext ||
+                window.webkitAudioContext)();
             const source = audioContextRef.current.createMediaElementSource(audioRef.current);
             analyserRef.current = audioContextRef.current.createAnalyser();
             source.connect(analyserRef.current);
@@ -51,26 +54,22 @@ function Musializer() {
         if (audioRef.current) {
             audioRef.current.volume = volume / 100;
         }
-        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener("keydown", handleKeyDown);
         return () => {
-            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener("keydown", handleKeyDown);
         };
     }, [volume, isPlaying]);
-    (0, react_2.useEffect)(() => {
-        const canvasDiv = document.getElementById("canvasDiv");
-        if (!canvasRef.current || !canvasDiv || !audioRef.current)
-            return;
-        const canvasDivRect = canvasDiv.getBoundingClientRect();
-        let ww = canvasDivRect.width;
-        let wh = canvasDivRect.height;
-        console.log(canvasDivRect.width, canvasDivRect.height);
+    (0, react_1.useEffect)(() => {
         const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d", { willReadFrequently: true, });
-        var particles = [];
+        const canvasDiv = document.getElementById("canvasDiv");
+        if (!canvas || !canvasDiv)
+            return;
+        const ctx = canvas.getContext("2d");
+        let particles = [];
+        let animationFrameId;
         let amount = 0;
-        let mouse = { x: ww / 2, y: wh / 2 };
+        let mouse = { x: canvas.width / 2, y: canvas.height / 2 };
         let bounceRadius = 1;
-        let radius = 0.5;
         const color = [
             getComputedStyle(document.documentElement).getPropertyValue("--particle-color"),
         ];
@@ -120,77 +119,39 @@ function Musializer() {
                 }
             }
         }
-        function resizeCanvas() {
-            const rectWidth = canvasDivRect.width;
-            const rectHeight = canvasDivRect.height;
-            canvas.width = rectWidth;
-            canvas.height = rectHeight;
-            const centerX = rectWidth / 2;
-            const centerY = (rectHeight / 2) - 100;
-            const particleSpacing = 20;
-            console.log(rectWidth, rectHeight);
-            particles = [];
-            for (let x = -rectWidth; x <= rectWidth; x += particleSpacing) {
-                for (let y = -rectHeight; y <= rectHeight; y += particleSpacing) {
-                    particles.push(new Particle(centerX + x, centerY + y));
-                }
-            }
-            amount = particles.length;
-        }
-        function initScene() {
-            // const rectWidth = canvasDivRect.width;
-            // const rectHeight = canvasDivRect.height;
-            // canvas.width = rectWidth;
-            // canvas.height = rectHeight;
-            // const centerX = rectWidth / 2;
-            // const centerY = (rectHeight / 2) - 100;
-            // const particleSpacing = 20;
-            // console.log(rectWidth,rectHeight)
-            // particles = [];
-            // for (let x = -rectWidth; x <= rectWidth; x+= particleSpacing) {
-            //     for (let y = -rectHeight; y <= rectHeight; y+= particleSpacing) {
-            //         particles.push(new Particle(centerX + x, centerY + y))
-            //     }
-            // }
-            // amount = particles.length
-            resizeCanvas();
-            window.addEventListener("resize", resizeCanvas);
-        }
-        const updateAndRender = () => {
+        const resizeCanvas = () => {
+            const rect = canvasDiv.getBoundingClientRect();
+            canvas.width = rect.width;
+            canvas.height = rect.height;
+        };
+        const render = () => {
             if (analyserRef.current) {
                 const dataArray = new Uint8Array(analyserRef.current.frequencyBinCount);
                 analyserRef.current.getByteFrequencyData(dataArray);
                 setAudioData(dataArray);
                 const bassRange = dataArray.slice(0, 2);
                 const intensity = bassRange.reduce((sum, value) => sum + value, 0);
-                const bass = intensity > 509;
-                setBass(bass);
-                bounceRadius = bass ? 2 : 0.5;
+                setBass(intensity > 509);
+                setBassIntensity(intensity);
             }
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            particles.forEach((particle) => {
-                particle.render();
-            });
-            requestAnimationFrame(updateAndRender);
+            animationFrameId = requestAnimationFrame(render);
         };
-        window.addEventListener("resize", initScene);
-        initScene();
-        requestAnimationFrame(updateAndRender);
+        window.addEventListener("resize", resizeCanvas);
+        resizeCanvas();
+        render();
         return () => {
-            window.removeEventListener("resize", initScene);
+            window.removeEventListener("resize", resizeCanvas);
+            cancelAnimationFrame(animationFrameId);
         };
-    }, [resetTrigger]);
-    function resetScene() {
-        setResetTrigger(prev => prev + 1);
-        console.log('reset');
-    }
+    }, [bassIntensity]);
     const handleKeyDown = (event) => {
-        if (event.code === 'Space') {
+        if (event.code === "Space") {
             event.preventDefault();
             handlePlayClick();
         }
     };
-    function handlePlayClick() {
+    const handlePlayClick = () => {
         var _a, _b;
         setIsPlaying(!isPlaying);
         if (isPlaying) {
@@ -202,25 +163,30 @@ function Musializer() {
         else {
             (_b = audioRef.current) === null || _b === void 0 ? void 0 : _b.pause();
         }
-    }
+    };
     return (react_1.default.createElement("div", { className: "bodyCenter" },
         react_1.default.createElement(framer_motion_1.motion.h1, null, "Musializer"),
-        react_1.default.createElement("div", { style: { display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems: 'center' } },
-            react_1.default.createElement(framer_motion_1.motion.button, { className: "playButton", style: { display: 'flex', justifyContent: 'center', alignItems: 'center' }, onMouseDown: handlePlayClick, animate: { scale: bass ? 1.5 : 1 }, transition: { type: "spring", duration: 0.2 } },
-                react_1.default.createElement("span", { className: "material-symbols-outlined", style: { fontSize: '50px' } }, isPlaying ? "play_arrow" : "pause")),
-            react_1.default.createElement("div", { style: { display: 'flex', flexDirection: 'column', paddingLeft: '50px' } },
+        react_1.default.createElement("div", { style: {
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+            } },
+            react_1.default.createElement(framer_motion_1.motion.button, { className: "playButton", style: {
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }, onMouseDown: handlePlayClick, animate: { scale: bass ? 1.5 : 1 }, transition: { type: "spring", duration: 0.2 } },
+                react_1.default.createElement("span", { className: "material-symbols-outlined", style: { fontSize: "50px" } }, isPlaying ? "play_arrow" : "pause")),
+            react_1.default.createElement("div", { style: {
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingLeft: "50px",
+                } },
                 react_1.default.createElement(Slider_1.Slider, { value: volume, set: setVolume }, "Volume"),
                 react_1.default.createElement(Slider_1.Slider, { value: bassIntensity, set: setBassIntensity }, "Intensity"),
                 react_1.default.createElement(Slider_1.Slider, { value: test, set: setTest }, "Test"))),
-        react_1.default.createElement("div", { style: { margin: '10px' } }),
-        react_1.default.createElement("div", { id: "canvasDiv", style: { height: '18rem', position: 'relative' } },
-            react_1.default.createElement("canvas", { ref: canvasRef, style: {
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                    // top: 0,
-                    // left: 0,
-                    // overflow: "hidden",
-                    // zIndex: -10,
-                } }))));
+        react_1.default.createElement("div", { style: { margin: "10px" } }),
+        react_1.default.createElement("div", { id: "canvasDiv", style: { height: "18rem", position: "relative" } },
+            react_1.default.createElement("canvas", { ref: canvasRef, style: { position: "absolute" } }))));
 }
