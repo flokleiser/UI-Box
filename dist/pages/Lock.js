7d7f1c1854d66c9fc0684a45558c3ca0c350c62c
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-09-12 13:19:10 +0000

    Deploying to gh-pages from @ flokleiser/UI-Box@72c45482f4dce806e617e2bd9ccc74a8038bf674 🚀

--- dist/pages/Lock.js
+++ dist/pages/Lock.js
@@ -24,7 +24,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Lock;
-//https://github.com/bobboteck/JoyStick?tab=readme-ov-file
 const react_1 = __importStar(require("react"));
 const framer_motion_1 = require("framer-motion");
 function Lock() {
@@ -34,12 +33,43 @@ function Lock() {
     const [dragStartAngle, setDragStartAngle] = (0, react_1.useState)(0);
     const [initialRotation, setInitialRotation] = (0, react_1.useState)(0);
     const [velocity, setVelocity] = (0, react_1.useState)(0);
-    const friction = 0.5;
-    const [lastTime, setLastTime] = (0, react_1.useState)(0);
-    const maxSpeed = 15;
-    //old mouse logic
+    const friction = 0.1;
+    let animationFrameId;
+    let resetAnimationFrameId;
+    // const [filledCircle, setFilledCircle] = useState(0);
+    // const [filledCircle1, setFilledCircle1] = useState(false);
+    // const [filledCircle2, setFilledCircle2] = useState(false);
+    // const [filledCircle3, setFilledCircle3] = useState(false);
+    // const [filledCircle4, setFilledCircle4] = useState(false);
+    const [filledCircleCount, setFilledCircleCount] = (0, react_1.useState)(0); // Change to state
     (0, react_1.useEffect)(() => {
-        let animationFrameId;
+        if (!isDragging && rotation !== 0) {
+            const resetRotation = () => {
+                setRotation((prevRotation) => {
+                    let newRotation = prevRotation - 4;
+                    if (newRotation < -360) {
+                        newRotation += 360;
+                    }
+                    if (Math.abs(newRotation) < 5 && Math.abs(newRotation) > -5) {
+                        return 0;
+                    }
+                    resetAnimationFrameId = requestAnimationFrame(resetRotation);
+                    return newRotation;
+                });
+            };
+            resetRotation();
+        }
+        else if (resetAnimationFrameId) {
+            cancelAnimationFrame(resetAnimationFrameId);
+        }
+        return () => {
+            if (resetAnimationFrameId) {
+                cancelAnimationFrame(resetAnimationFrameId);
+            }
+        };
+    }, [isDragging]);
+    (0, react_1.useEffect)(() => {
+        // let animationFrameId:number;
         const updateRotation = () => {
             setVelocity((prevVelocity) => {
                 const newVelocity = prevVelocity * friction;
@@ -52,31 +82,43 @@ function Lock() {
             animationFrameId = requestAnimationFrame(updateRotation);
         };
         updateRotation();
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseup', handleMouseUp);
         return () => {
             cancelAnimationFrame(animationFrameId);
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseup', handleMouseUp);
         };
-    }, []);
+    }, [isDragging, dragStartAngle, initialRotation]);
     const calculateAngle = (x, y) => {
         if (!lockRef.current)
             return 0;
         const rect = lockRef.current.getBoundingClientRect();
-        const spinnerX = rect.left + rect.width / 2;
-        const spinnerY = rect.top + rect.height / 2;
-        return Math.atan2(y - spinnerY, x - spinnerX) * (180 / Math.PI);
+        const lockX = rect.left + rect.width / 2;
+        const lockY = rect.top + rect.height / 2;
+        return Math.atan2(y - lockY, x - lockX) * (180 / Math.PI);
+    };
+    const fillCircles = () => {
+        setFilledCircleCount((prevCount) => {
+            if (prevCount < 4) {
+                return prevCount + 1;
+            }
+            else {
+                return 0;
+            }
+        });
     };
     const handleMouseDown = (e) => {
         setIsDragging(true);
         const angle = calculateAngle(e.clientX, e.clientY);
         setDragStartAngle(angle);
         setInitialRotation(rotation);
-        setLastTime(Date.now());
     };
     const handleMouseMove = (e) => {
         if (isDragging) {
             const currentAngle = calculateAngle(e.clientX, e.clientY);
             let angleDiff = currentAngle - dragStartAngle;
-            const currentTime = Date.now();
-            const timeDiff = (currentTime - lastTime);
+            // let newRotation = initialRotation + angleDiff;
             if (e.clientX < window.innerWidth / 2) {
                 angleDiff = -angleDiff;
                 setRotation(initialRotation - angleDiff);
@@ -84,32 +126,40 @@ function Lock() {
             else {
                 setRotation(initialRotation + angleDiff);
             }
-            if (timeDiff > 0) {
-                const newVelocity = Math.min(maxSpeed, Math.max(-maxSpeed, angleDiff / timeDiff));
-                setVelocity((newVelocity) / 4);
-            }
-            setLastTime(currentTime);
         }
     };
     const handleMouseUp = () => {
         setIsDragging(false);
     };
-    (0, react_1.useEffect)(() => {
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mouseup', handleMouseUp);
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, [isDragging, dragStartAngle, initialRotation, lastTime]);
     return (react_1.default.createElement("div", { className: "bodyCenter" },
-        react_1.default.createElement("div", null,
-            react_1.default.createElement("h1", null, "Lock")),
+        react_1.default.createElement("div", { style: { display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' } },
+            react_1.default.createElement("h1", null, "Lock"),
+            react_1.default.createElement("div", { style: { display: 'flex', flexDirection: 'row' } },
+                react_1.default.createElement("div", { className: "navbarButton", style: { backgroundColor: 'rgba(0,0,0,0)', } },
+                    react_1.default.createElement("span", { className: "material-symbols-outlined" },
+                        " ",
+                        filledCircleCount >= 1 ? 'radio_button_checked' : 'radio_button_unchecked',
+                        " ")),
+                react_1.default.createElement("div", { className: "navbarButton", style: { backgroundColor: 'rgba(0,0,0,0)', } },
+                    react_1.default.createElement("span", { className: "material-symbols-outlined" },
+                        " ",
+                        filledCircleCount >= 2 ? 'radio_button_checked' : 'radio_button_unchecked',
+                        " ")),
+                react_1.default.createElement("div", { className: "navbarButton", style: { backgroundColor: 'rgba(0,0,0,0)', } },
+                    react_1.default.createElement("span", { className: "material-symbols-outlined" },
+                        " ",
+                        filledCircleCount >= 3 ? 'radio_button_checked' : 'radio_button_unchecked',
+                        " ")),
+                react_1.default.createElement("div", { className: "navbarButton", style: { backgroundColor: 'rgba(0,0,0,0)', } },
+                    react_1.default.createElement("span", { className: "material-symbols-outlined" },
+                        " ",
+                        filledCircleCount === 4 ? 'radio_button_checked' : 'radio_button_unchecked',
+                        " ")))),
         react_1.default.createElement("div", { style: { justifyContent: 'center', alignItems: 'center', display: 'flex' } },
             react_1.default.createElement("div", { className: 'lockDiv' },
                 react_1.default.createElement(framer_motion_1.motion.div, { className: "lock", ref: lockRef, onMouseDown: handleMouseDown, style: { transform: `rotate(${rotation}deg)` } },
                     react_1.default.createElement("div", { className: "lockCenter1", style: { top: '50%', left: '50%' } }),
-                    react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '74.75%', left: '74.75%', borderRadius: '0 0 230px 0', width: 200, height: 200 } }),
+                    react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '74.75%', left: '74.75%', borderRadius: '0 0 230px 0', width: 200, height: 200, pointerEvents: 'none' } }),
                     react_1.default.createElement("div", { className: "smallerLockCircleInvert", style: { top: '57%', left: '84.75%', width: 115, height: 57.5, borderRadius: '0px 0px 57.5px 57.5px' } }),
                     react_1.default.createElement("div", { className: "smallerLockCircleInvert", style: { top: '84.75%', left: '57%', height: 115, width: 57.5, borderRadius: '0px 57.5px 57.5px 0px' } }),
                     react_1.default.createElement("div", { className: "lockCenter2", style: { top: '50%', left: '50%' } }),
@@ -123,5 +173,17 @@ function Lock() {
                     react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '32.5%', left: '20%' } }),
                     react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '19.5%', left: '32.5%' } }),
                     react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '80.5%', left: '32.5%' } }),
-                    react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '19.5%', left: '67.5%' } }))))));
+                    react_1.default.createElement("div", { className: "smallerLockCircle", style: { top: '19.5%', left: '67.5%' } })),
+                react_1.default.createElement("div", { className: "smallerLockCircleInvert", style: { top: '75%', left: '75%', width: 55, height: 55, borderRadius: '50%',
+                    }, onMouseOver: () => { isDragging ? fillCircles() : ' '; } }),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '50%', left: '85%', pointerEvents: 'none' } }, "1"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '85%', left: '50%', pointerEvents: 'none' } }, "0"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '80.5%', left: '32.5%', pointerEvents: 'none' } }, "9"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '67.5%', left: '20%', pointerEvents: 'none' } }, "8"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '50%', left: '15%', pointerEvents: 'none' } }, "7"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '32.5%', left: '20%', pointerEvents: 'none' } }, "6"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '19.5%', left: '32.5%', pointerEvents: 'none' } }, "5"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '15%', left: '50%', pointerEvents: 'none' } }, "4"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '19.5%', left: '67.5%', pointerEvents: 'none' } }, "3"),
+                react_1.default.createElement("div", { className: "lockText", style: { top: '32.5%', left: '80%', pointerEvents: 'none' } }, "2")))));
 }
--- pages/Lock.tsx
+++ pages/Lock.tsx
@@ -1,7 +1,5 @@
-//https://github.com/bobboteck/JoyStick?tab=readme-ov-file
 import React, { useEffect, useState, useRef } from 'react';
-import {motion, useSpring, useTransform, useAnimation, useDragControls, useMotionValue} from "framer-motion"
-
+import {motion, useSpring, useTransform, useAnimation, useDragControls, useMotionValue, animate} from "framer-motion"
 
 export default function Lock() {
     const lockRef= useRef<HTMLDivElement>(null);
@@ -13,89 +11,142 @@ export default function Lock() {
     const [initialRotation, setInitialRotation] = useState(0);
 
     const [velocity, setVelocity] = useState(0);
-    const friction = 0.5;
+    const friction = 0.1;
 
-    const [lastTime, setLastTime] = useState(0);
-    const maxSpeed = 15
-
-
-    //old mouse logic
-useEffect(() => {
     let animationFrameId:number;
-    const updateRotation = () => {
-        setVelocity((prevVelocity) => {
-            const newVelocity = prevVelocity * friction;
-            if (Math.abs(newVelocity) < 0.001) {
-                return 0;
+    let resetAnimationFrameId:number;
+
+    // const [filledCircle, setFilledCircle] = useState(0);
+    // const [filledCircle1, setFilledCircle1] = useState(false);
+    // const [filledCircle2, setFilledCircle2] = useState(false);
+    // const [filledCircle3, setFilledCircle3] = useState(false);
+    // const [filledCircle4, setFilledCircle4] = useState(false);
+    const [filledCircleCount, setFilledCircleCount] = useState(0); // Change to state
+
+    useEffect(() => {
+        if (!isDragging && rotation !== 0) {
+            const resetRotation = () => {
+                setRotation((prevRotation) => {
+                    let newRotation = prevRotation - 4;
+                    if (newRotation < -360) {
+                        newRotation += 360;
+                    }
+                    if (Math.abs(newRotation) < 5 && Math.abs(newRotation) > -5) {
+                        return 0;
+                    }
+                    resetAnimationFrameId = requestAnimationFrame(resetRotation);
+                    return newRotation;
+                });
+            };
+            resetRotation();
+        } else if (resetAnimationFrameId) {
+            cancelAnimationFrame(resetAnimationFrameId);
+        }
+        return () => {
+            if (resetAnimationFrameId) {
+                cancelAnimationFrame(resetAnimationFrameId);
             }
-            setRotation((prevRotation) => prevRotation + newVelocity);
-            return newVelocity;
-        });
-        animationFrameId = requestAnimationFrame(updateRotation);
+        };
+    }, [isDragging]); 
+
+    useEffect(() => {
+        // let animationFrameId:number;
+        const updateRotation = () => {
+            setVelocity((prevVelocity) => {
+                const newVelocity = prevVelocity * friction;
+                if (Math.abs(newVelocity) < 0.001) {
+                    return 0;
+                }
+                setRotation((prevRotation) => prevRotation + newVelocity);
+                return newVelocity;
+            });
+
+            animationFrameId = requestAnimationFrame(updateRotation);
+        };
+        updateRotation();
+
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseup', handleMouseUp);
+        return () => {
+            cancelAnimationFrame(animationFrameId);
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [isDragging, dragStartAngle, initialRotation]);
+
+    const calculateAngle = (x:number, y:number) => {
+        if (!lockRef.current) return 0;
+        const rect = lockRef.current.getBoundingClientRect();
+        const lockX = rect.left + rect.width / 2;
+        const lockY = rect.top + rect.height / 2;
+
+        return Math.atan2(y - lockY, x - lockX) * (180 / Math.PI);
     };
-    updateRotation();
-    return () => {
-        cancelAnimationFrame(animationFrameId);
-    };
-}, []);
-const calculateAngle = (x:number, y:number) => {
-    if (!lockRef.current) return 0;
-    const rect = lockRef.current.getBoundingClientRect();
-    const spinnerX = rect.left + rect.width / 2;
-    const spinnerY = rect.top + rect.height / 2;
 
-    return Math.atan2(y - spinnerY, x - spinnerX) * (180 / Math.PI);
-};
-const handleMouseDown = (e:React.MouseEvent<HTMLDivElement>) => {
-    setIsDragging(true);
-    const angle = calculateAngle(e.clientX, e.clientY);
-
-    setDragStartAngle(angle);
-    setInitialRotation(rotation);
-    setLastTime(Date.now());
-};
-const handleMouseMove = (e:MouseEvent) => {
-    if (isDragging) {
-        const currentAngle = calculateAngle(e.clientX, e.clientY);
-        let angleDiff = currentAngle - dragStartAngle;
-        const currentTime = Date.now();
-        const timeDiff = (currentTime - lastTime); 
-
-
-        if (e.clientX < window.innerWidth / 2) {
-            angleDiff = -angleDiff
-            setRotation(initialRotation - angleDiff);
-        } else {
-            setRotation(initialRotation + angleDiff);
-        }
-
-        if (timeDiff > 0) {
-            const newVelocity = Math.min(maxSpeed, Math.max(-maxSpeed, angleDiff / timeDiff));
-            setVelocity((newVelocity)/4);
-        }
-        setLastTime(currentTime);
+    const fillCircles = () => {
+        setFilledCircleCount((prevCount) => {
+            if (prevCount < 4) {
+                return prevCount + 1
+            }else {
+                return 0
+            }})
     }
-};
-const handleMouseUp = () => {
-    setIsDragging(false);
-};
-useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
-    return () => {
-    window.removeEventListener('mousemove', handleMouseMove);
-    window.removeEventListener('mouseup', handleMouseUp);
+
+
+    const handleMouseDown = (e:React.MouseEvent<HTMLDivElement>) => {
+        setIsDragging(true);
+        const angle = calculateAngle(e.clientX, e.clientY);
+
+        setDragStartAngle(angle);
+        setInitialRotation(rotation);
     };
-}, [isDragging, dragStartAngle, initialRotation, lastTime]);
+
+    const handleMouseMove = (e:MouseEvent) => {
+        if (isDragging) {
+            const currentAngle = calculateAngle(e.clientX, e.clientY);
+            let angleDiff = currentAngle - dragStartAngle;
+            // let newRotation = initialRotation + angleDiff;
+            if (e.clientX < window.innerWidth / 2) {
+                angleDiff = -angleDiff
+                setRotation(initialRotation - angleDiff);
+            } else {
+                setRotation(initialRotation + angleDiff);
+            }
+        }
+    };
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
 
     return (
         <div className="bodyCenter">
-        <div>
+        {/* <div> */}
+        <div style={{display: 'flex',flexDirection: 'row',justifyContent: 'space-between',alignItems: 'center'}}>
             <h1>Lock</h1>
+
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <div className="navbarButton" style={{backgroundColor:'rgba(0,0,0,0)', }} >
+                {/* <span className="material-symbols-outlined"> {filledCircle1 ? 'radio_button_unchecked' : 'radio_button_checked'} </span> */}
+                <span className="material-symbols-outlined"> {filledCircleCount >= 1? 'radio_button_checked' : 'radio_button_unchecked'} </span>
+            </div>
+            <div className="navbarButton" style={{backgroundColor:'rgba(0,0,0,0)', }} >
+                {/* <span className="material-symbols-outlined"> radio_button_unchecked </span> */}
+                <span className="material-symbols-outlined"> {filledCircleCount >= 2? 'radio_button_checked' : 'radio_button_unchecked'} </span>
+            </div>
+            <div className="navbarButton" style={{backgroundColor:'rgba(0,0,0,0)', }} >
+                {/* <span className="material-symbols-outlined"> radio_button_unchecked </span> */}
+                <span className="material-symbols-outlined"> {filledCircleCount >= 3? 'radio_button_checked' : 'radio_button_unchecked'} </span>
+            </div>
+            <div className="navbarButton" style={{backgroundColor:'rgba(0,0,0,0)', }} >
+                {/* <span className="material-symbols-outlined"> radio_button_unchecked </span> */}
+                <span className="material-symbols-outlined"> {filledCircleCount === 4? 'radio_button_checked' : 'radio_button_unchecked'} </span>
+            </div>
+        </div>
         </div>
 
 
         <div style={{justifyContent:'center', alignItems:'center', display:'flex'}}>
+
         <div className='lockDiv'>
         <motion.div className="lock"
             ref={lockRef}
@@ -104,7 +155,7 @@ useEffect(() => {
         >
 
             <div className="lockCenter1" style={{ top: '50%', left: '50%' }}></div>
-            <div className="smallerLockCircle" style={{top: '74.75%', left : '74.75%', borderRadius:'0 0 230px 0',width:200,height:200}} />
+            <div className="smallerLockCircle" style={{top: '74.75%', left : '74.75%', borderRadius:'0 0 230px 0',width:200,height:200, pointerEvents:'none'}} />
 
             <div className="smallerLockCircleInvert" style={{ top: '57%', left: '84.75%',  width:115,height:57.5, borderRadius:'0px 0px 57.5px 57.5px'}} />
             <div className="smallerLockCircleInvert" style={{ top: '84.75%', left: '57%', height:115,width:57.5, borderRadius:'0px 57.5px 57.5px 0px'}} />
@@ -112,12 +163,10 @@ useEffect(() => {
             <div className="lockCenter2" style={{top: '50%', left: '50%'}}></div>
             <div className="lockCenter1" style={{width:165,height:165,top: '50%', left: '50%' }}></div>
 
-            <div className="smallerLockCircle" style={{ top: '50%', left: '15%' }} />
+            <div className="smallerLockCircle" style={{ top: '50%', left: '15%' }}/>
             <div className="smallerLockCircle" style={{ top: '15%', left: '50%' }} />
-
             <div className="smallerLockCircle" style={{ top: '50%', left: '85%' }} />
             <div className="smallerLockCircle" style={{ top: '85%', left: '50%' }} />
-
             <div className="smallerLockCircle" style={{ top: '32.5%', left: '80%' }} />
             <div className="smallerLockCircle" style={{ top: '67.5%', left: '20%' }} />
             <div className="smallerLockCircle" style={{ top: '32.5%', left: '20%' }} />
@@ -125,11 +174,46 @@ useEffect(() => {
             <div className="smallerLockCircle" style={{ top: '80.5%', left: '32.5%' }} />
             <div className="smallerLockCircle" style={{ top: '19.5%', left: '67.5%' }} />
 
-
-
         </motion.div>
 
+            <div className="smallerLockCircleInvert" style={{top: '75%', left : '75%', width:55,height:55, borderRadius:'50%', 
+            }} 
+            onMouseOver={() =>  {isDragging? fillCircles() :' ' }}
+            />
+
+            <div className="lockText" style={{ top: '50%', left: '85%', pointerEvents:'none'}} >
+               1 
+            </div>
+            <div className="lockText" style={{ top: '85%', left: '50%', pointerEvents:'none'}} >
+                0 
+            </div>
+            <div className="lockText" style={{ top: '80.5%', left: '32.5%',pointerEvents:'none'}} >
+               9 
+            </div>
+            <div className="lockText" style={{ top: '67.5%', left: '20%',pointerEvents:'none'}} >
+               8 
+            </div>
+            <div className="lockText" style={{ top: '50%', left: '15%',pointerEvents:'none'}}>
+               7 
+            </div>
+            <div className="lockText" style={{ top: '32.5%', left: '20%',pointerEvents:'none'}} >
+               6 
+            </div>
+            <div className="lockText" style={{ top: '19.5%', left: '32.5%',pointerEvents:'none'}} >
+               5 
+            </div>
+            <div className="lockText" style={{ top: '15%', left: '50%', pointerEvents:'none'}} >
+               4 
+            </div>
+            <div className="lockText" style={{ top: '19.5%', left: '67.5%',pointerEvents:'none'}} >
+               3 
+            </div>
+            <div className="lockText" style={{ top: '32.5%', left: '80%',pointerEvents:'none'}} >
+                2 
+            </div>
+
         </div>
+
         </div>
 
         </div>
