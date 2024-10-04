aed98d34ede5ee147d240d73ef48d87c3d39d25e
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-10-04 18:59:12 +0000

    Deploying to gh-pages from @ flokleiser/UI-Box@655472191c1206ac7218be28f07b0b09d052722b 🚀

---
+++ pages/Lights.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { motion, useSpring, useMotionValue, useTransform, animate, transform } from "framer-motion";
+
+export default function Lights() {
+   
+    return (
+        <div className="bodyCenter">
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'start', alignItems:'center'}}> 
+                <h1>Lights</h1>
+
+
+            </div>
+
+        </div>
+    );
+}
